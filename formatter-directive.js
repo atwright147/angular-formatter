3b20a9573dc5cff61679c85a78e9fc2511ca250c
Andy Wright <atwright147@gmail.com>
2015-09-03 22:40:52 +0100

    add directive and demo page

---
+++ formatter-directive.js
@@ -0,0 +1,30 @@
+/**
+* formatter Module
+*
+* Description
+*/
+angular.module('formatter', []).directive('formatter', function(){
+	// Runs during compile
+	return {
+		// name: '',
+		// priority: 1,
+		// terminal: true,
+		// scope: {}, // {} = isolate, true = child, false/undefined = no change
+		// controller: function($scope, $element, $attrs, $transclude) {},
+		require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+		restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+		// template: '',
+		// templateUrl: '',
+		// replace: true,
+		// transclude: true,
+		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+		link: function($scope, elm, attrs, ngModel) {
+			var _pattern    = elm.attr('formatter-pattern') || '';
+			var _persistent = attrs.formatterPersistent ? true : false;
+			var formatted   = new Formatter(elm[0], {
+				'pattern': _pattern,
+				'persistent': _persistent
+			});
+		}
+	};
+});
